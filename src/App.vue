ef96e0eb498a485aab16db74822a2e0d3a87af11
Hung Anh <anhlhpl52119@gmail.com>
2023-07-18 22:18:26 +0700

    feat: imp tailwind

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,11 @@
 <template>
-    <RouterView #="{ Component }">
+    <!-- <RouterView #="{ Component }">
         <component :is="Component" />
-    </RouterView>
+    </RouterView> -->
+    <div class="bg-red-30">
+        hello
+    </div>
 </template>
-<script lang="ts" setup></script>
+<script lang="ts" setup>
+</script>
 <style scoped></style>
